4cc4156fe252da1f3e0abe55887cda838744a77a
Abhishek Singh <128680162+imabhisingh@users.noreply.github.com>
2023-08-09 11:39:53 +0530

    Create videocall.php

---
+++ myapps/videocall.php
@@ -0,0 +1,69 @@
+{
+  "ad_ids": [
+    {
+      "ad_network": "Admob",
+      "unit_type": "Banner",
+      "placement_id": "ca-app-pub-3940256099942544/6300978111",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Interstitial",
+      "placement_id": "ca-app-pub-3940256099942544/1033173712",
+      "priority": "1",
+      "counter": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Native",
+      "placement_id": "ca-app-pub-3940256099942544/2247696110",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "RewardAd",
+      "placement_id": "ca-app-pub-3940256099942544/5224354917",
+      "priority": "0"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "AppOpenAd",
+      "placement_id": "ca-app-pub-3940256099942544/3419835294",
+      "priority": "1"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Banner",
+      "placement_id": "d82261f7ee24e5b6",
+      "priority": "0"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Interstitial",
+      "placement_id": "2b11f67e57032afa",
+      "priority": "0",
+      "counter": "1"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Native",
+      "placement_id": "7a458df55d76a8b5",
+      "priority": "0"
+    }
+  ],
+  "info": [
+    {
+      "type": "random_call_video",
+      "count": 50,
+      "is_show": "yes"
+    }
+  ],
+  "webrtc": [
+    {
+      "provider": "agora",
+      "app_id": "8079664ca5fe4cb086b4dd11bd948176",
+      "certificate": "34b5846cbdf14a6e85d9ae4601e119ab",
+      "channel": "videocall"
+    }
+  ]
+}
